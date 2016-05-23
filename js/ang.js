2b8b8214fba579be0d832a30880f7f7d3d888c25
nodes777 <nodes777@gmail.com>
2016-05-23 10:01:27 -0400

    gets images and names loaded on init

--- js/ang.js
+++ js/ang.js
@@ -1,11 +1,46 @@
 angular.module("root", [])
     .controller("rightAnimal", ["$scope", function($scope){
-       $scope.name = "right";
+    	var self = this;
+    	var name;
+    	var animalImage;
+
+       function newAnimal() {
+       	var randNum = Math.floor(Math.random()*animalPool.length);
+       	var animal = animalPool[randNum];
+       	animalImage = animal.img;
+       	name = animal.name;
+       	animalPool.slice(randNum);
+       };
+       newAnimal();
+
+       $scope.name = name;
+       $scope.img = animalImage;
     }])
     .controller("leftAnimal", ["$scope", function($scope){
-       $scope.name = "left";
+     	var self = this;
+    	var name;
+    	var animalImage;
+
+       function newAnimal() {
+       	var randNum = Math.floor(Math.random()*animalPool.length);
+       	var animal = animalPool[randNum];
+       	animalImage = animal.img;
+       	name = animal.name;
+       	animalPool.slice(randNum);
+       };
+       newAnimal();
+
+       $scope.name = name;
+       $scope.img = animalImage;
     }]);
 
+
+
+var Animal = function(data) {
+    this.name = data.name
+    this.img = data.img;
+    this.baby = data.baby;
+};
 
 var animals =[{
 	name : "Baby Quetzal",
@@ -17,7 +52,16 @@ var animals =[{
 	baby: true
 },{
 	name : "Baby Octopus",
-	img: "http://i.imgur.com/kzarlKW.jpg"
+	img: "http://i.imgur.com/kzarlKW.jpg",
 	baby: true
+}];
+
+var animalPool = [];
+
+var init = function(){
+	 animals.forEach(function(animalData) {
+        animalPool.push(new Animal(animalData));
+        });
 }
-]
+init();
+console.log(animalPool);
