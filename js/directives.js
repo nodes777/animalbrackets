e6edee23ee100a7d007196836c878ab2971fe751
nodes777 <nodes777@gmail.com>
2016-11-04 17:35:36 -0700

    fades, but changes image then fades

--- js/directives.js
+++ js/directives.js
@@ -7,6 +7,7 @@ app.directive("win", function() {
     controller: 'winner'
   };
 });
+/*
 app.directive('animateOnChange', function($timeout) {
   return function(scope, element, attr) {
     scope.$watch(attr.animateOnChange, function(nv,ov) {
@@ -19,3 +20,40 @@ app.directive('animateOnChange', function($timeout) {
     });
   };
 });
+*/
+/*
+app.directive('fadeIn', function($timeout){
+   return function($scope, $element, attr) {
+    $scope.$watch(attr, function(nv,ov) {
+      console.log(attr.ngSrc);
+      if (nv!=ov) {
+        console.log('change noticed')
+        element.addClass('changed');
+        $timeout(function() {
+          element.removeClass('changed');
+        }, 1000); // Could be enhanced to take duration as a parameter
+      }
+    });
+  };
+});
+*/
+
+app.directive('fadeIn', function($timeout){
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+          attrs.$observe("ngSrc", function () {
+              console.log("change")
+                element.removeClass("showy");
+                element.addClass("hidey");
+            })
+            element.on("load", function () {
+                $timeout(function () {
+                    element.removeClass("hidey");
+                    element.addClass("showy");
+                }, 500);
+            });
+            
+        }
+    }
+})
